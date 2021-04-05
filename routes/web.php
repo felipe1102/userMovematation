2b2c7bfd48941487be629e21ff0bd7164fdcea3e
Felipe Moreira de Castro <felipemoreiradecastro@gmail.com>
2021-04-05 16:30:10 -0300

    ajuste arquivo rotas web

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    //return view('welcome');
+    return response()->json(['message' => 'API teste', 'created by' => 'Felipe Moreira de Castro']);
 });
+
